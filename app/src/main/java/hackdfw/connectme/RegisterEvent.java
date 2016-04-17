2d0202408490c6e87e44732a3682e25f65a8d50e
Manu Bhat <manubhat@outlook.com>
2016-04-17 01:12:39 -0500

    Edit : Register Event page

--- app/src/main/java/hackdfw/connectme/RegisterEvent.java
+++ app/src/main/java/hackdfw/connectme/RegisterEvent.java
@@ -18,13 +18,16 @@ import java.util.Random;
 
 public class RegisterEvent extends AppCompatActivity {
 
-    private static final String LOG_TAG = RegisterEvent.class.getSimpleName();
-
+   // public final int PICK_CONTACT = 2015;
+    private static final String TAG = RegisterEvent.class.getSimpleName();
+    private static final int REQUEST_CODE_PICK_CONTACTS = 1;
+    private Uri uriContact;
+    private String contactID;     // contacts unique ID
+    int eventID;
     public static final int REQUEST_CODE_PICK_CONTACT = 1;
     public static final int MAX_PICK_CONTACT = 10;
 
     public final int PICK_CONTACT = 2015;
-    private int eventID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +44,7 @@ public class RegisterEvent extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
-                startActivityForResult(i, PICK_CONTACT);
+                startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI), REQUEST_CODE_PICK_CONTACTS);
 
             }
         });
@@ -50,37 +53,96 @@ public class RegisterEvent extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK) {
-            EditText contNumb = (EditText) findViewById(R.id.contNumb);
-            EditText contName = (EditText) findViewById(R.id.contName);
-            String contactName = null;
-            Uri contactUri = data.getData();
-            Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
-            while (cursor.moveToNext()) {
-                contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
-            }
-            cursor.moveToFirst();
-            int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-            Log.d("phone number", cursor.getString(column));
-            contName.setText(contactName);
-            contNumb.setText(cursor.getString(column));
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_PICK_CONTACTS && resultCode == RESULT_OK) {
+            Log.d(TAG, "Response: " + data.toString());
+            uriContact = data.getData();
+
+            retrieveContactName();
+            retrieveContactNumber();
+        }
+    }
+
+
+
+    private void retrieveContactNumber() {
+
+        String contactNumber = null;
+        EditText contNumb = (EditText)findViewById(R.id.contNumb);
+
+        // getting contacts ID
+        Cursor cursorID = getContentResolver().query(uriContact,
+                new String[]{ContactsContract.Contacts._ID},
+                null, null, null);
+
+        if (cursorID.moveToFirst()) {
+
+            contactID = cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts._ID));
         }
 
-        if (resultCode == RESULT_OK) {
+        cursorID.close();
 
-            if (requestCode == REQUEST_CODE_PICK_CONTACT) {
+        Log.d(TAG, "Contact ID: " + contactID);
 
-                Bundle bundle = data.getExtras();
+        // Using the contact ID now we will get contact phone number
+        Cursor cursorPhone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
 
-                String result = bundle.getString("result");
-                ArrayList<String> contacts = bundle.getStringArrayList("result");
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE + " = " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
 
-                Log.d(LOG_TAG, "launchMultiplePhonePicker bundle.toString()= " + contacts.get(0).toString());
+                new String[]{contactID},
+                null);
 
-            }
-
-            super.onActivityResult(requestCode, resultCode, data);
+        if (cursorPhone.moveToFirst()) {
+            contactNumber = cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
         }
+
+        cursorPhone.close();
+        contNumb.setText(contactNumber);
+        Log.d(TAG, "Contact Phone Number: " + contactNumber);
+    }
+
+    private void retrieveContactName() {
+
+        String contactName = null;
+        EditText contName = (EditText)findViewById(R.id.contName);
+        // querying contact data store
+        Cursor cursor = getContentResolver().query(uriContact, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+
+            // DISPLAY_NAME = The display name for the contact.
+            // HAS_PHONE_NUMBER =   An indicator of whether this contact has at least one phone number.
+
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        }
+
+        cursor.close();
+        contName.setText(contactName);
+        Log.d(TAG, "Contact Name: " + contactName);
 
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK) {
+//            EditText contNumb = (EditText)findViewById(R.id.contNumb);
+//            EditText contName = (EditText)findViewById(R.id.contName);
+//            String contactName = null;
+//            Uri contactUri = data.getData();
+//            Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+//            while(cursor.moveToNext()){
+//                contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
+//            }
+//            cursor.moveToFirst();
+//            int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+//            Log.d("phone number", cursor.getString(column));
+//            contName.setText(contactName);
+//            contNumb.setText(cursor.getString(column));
+//        }
+//    }
+
 }
